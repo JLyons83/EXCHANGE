8865d2023a94e027416765c2d1e8877a1b241015
JLyons83 <67870719+JLyons83@users.noreply.github.com>
2025-02-02 17:41:58 +0200

    Add files via upload

--- new-mailuser.ps1
+++ new-mailuser.ps1
@@ -6,15 +6,13 @@ foreach($user in $users){
 
 
     #delete emailaddresses from distribution list
-
-
-   
+       
         foreach($column in $user.PSObject.Properties.Name) {
 
              # Check if the value is equal to 1 and therefore delete from distribution list
 
             if ($user.$column -eq 1) {
-                Write-Output "Column '$column' in row '$($user.name)' is TRUE, performing action..."
+                Write-host "deleting $user.externalemailaddress1 from distribution list $column"
                 Remove-DistributionGroupMember -identity $column -member $user.externalemailaddress1 -confirm:$false
                          }
 
@@ -30,7 +28,7 @@ foreach($user in $users){
         foreach ($column in $user.PSObject.Properties.Name){
             # Check if the value is $true
             if ($user.$column -eq 1) {
-
+            Write-host "deleting $user.externalemailaddress1 from contacts"
                 remove-mailcontact -identity $($user.externalemailaddress1) -confirm:$false
 
                 #breaks loop once finds 1 column equal to 1
@@ -41,6 +39,24 @@ foreach($user in $users){
     #sleep to guarantee deletion of contact before creation of mail user
     start-sleep -Seconds 2
 
+     #check if user has user in 365 and then add mailbox with forwarding rule
+     try{
+
+        $365user = get-mguser -userid $user.MicrosoftOnlineServicesID -erroraction SilentlyContinue
+
+            if($365user){
+
+                Set-MgUserLicense -userid $user.MicrosoftOnlineServicesID -AddLicenses @{SkuId = "78e66a63-337a-4a9a-8959-41c6654dfb56"} -RemoveLicenses @()
+
+                    start-sleep -Seconds 50
+
+                set-mailbox -Identity $user.MicrosoftOnlineServicesID -ForwardingSmtpAddress $user.externalemailaddress1 -DeliverToMailboxAndForward $false
+                }
+     else{
+           
+      #if not user in 365 then create mailuser
+
+      
     #Randomly Chooses Password
     Add-Type -AssemblyName 'System.Web'
     $minLength = 20 ## characters
@@ -79,4 +95,11 @@ foreach($user in $users){
 
  
  }
+
+
+      
+
+     }
+     catch{}
+     }
  
